0c8fb0558bb4b8880b8b453fb64bc36e4511c4be
svennela <sridhar.vennela@gmail.com>
2018-11-08 10:34:44 -0800

    first commit

---
+++ src/main/java/com/pivotal/consul/democonsul/DemoConsulApplication.java
@@ -0,0 +1,12 @@
+package com.pivotal.consul.democonsul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoConsulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoConsulApplication.class, args);
+	}
+}
